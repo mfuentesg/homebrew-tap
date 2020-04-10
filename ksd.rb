77d1edf4d3f25fe9dd0d072e4e329cc18b8e7923
mfuentesg <marceloe.fuentes@gmail.com>
2020-04-10 00:48:03 -0400

    Brew formula update for ksd version v1.0.6

--- ksd.rb
+++ ksd.rb
@@ -7,11 +7,11 @@ class Ksd < Formula
 
   if OS.mac?
     url "https://github.com/mfuentesg/ksd/releases/download/v1.0.6/ksd_1.0.6_Darwin_x86_64.tar.gz"
-    sha256 "0ebbf0f7fd7019c5c9ebe0f738f457fb50633fe82e105632972dc70b30bd65bd"
+    sha256 "2243460d4d2ceee4c9e54bcbdc7a0b647b83e8d3fd7b3b167d4c93ff0c9ac324"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/mfuentesg/ksd/releases/download/v1.0.6/ksd_1.0.6_Linux_x86_64.tar.gz"
-      sha256 "d55888e72065756e5f4c15f5a3601706651a20f747ed4a359a80f378eb14b8e4"
+      sha256 "40d05bc3c3a1564978939a2dd750f03f7c0ad36101be63d8d0b1f2f1cf1111d5"
     end
   end
 
